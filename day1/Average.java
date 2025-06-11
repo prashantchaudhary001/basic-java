2443213a0e4499c2b2367e0763c577dfda6ec2ce
prashantchaudhary001 <jaatprashant55@gmail.com>
2025-06-11 11:32:35 +0530

    second commit

---
+++ day1/Average.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Average{
+   public static void main(String[] args){		
+	int physics=95;
+	int math=94;
+	int chemistry=96;
+	int total=95+94+96;
+	float avg=total/3;
+	System.out.println(avg);
+}
+}
