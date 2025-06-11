8fa0ddbbfc72873efdb7d7c4df2ffadfcb9f3bc7
prashantchaudhary001 <jaatprashant55@gmail.com>
2025-06-11 12:14:18 +0530

    fifth commit

---
+++ day1/Program5.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Program5{
+   public static void main(String[] args){		
+	int noOfPens=14;
+	int students=3;
+	int getPen=noOfPens/students;
+	int remaining=14%3;
+	
+	System.out.println("the pen per student is"+""+getPen+"and the remaining are"+""+remaining);
+}
+}
