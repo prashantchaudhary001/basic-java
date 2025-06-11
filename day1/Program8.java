b9df9babbef558f0370331e059524fd1a1b2fc69
prashantchaudhary001 <jaatprashant55@gmail.com>
2025-06-11 13:30:23 +0530

    eight commit

---
+++ day1/Program8.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Program8
+{
+	public static void main(String[] args) {
+	Scanner input=new Scanner(System.in);
+        double km = input.nextDouble();
+        double miles = 1.6*km;
+        
+        System.out.println("total miles is"+""+miles+"mile for the given"+""+km+"km");
+	}
+}
