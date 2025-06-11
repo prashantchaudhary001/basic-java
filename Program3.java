9e335fe70df0ded8bc7970289dc68a238048ce08
prashantchaudhary001 <jaatprashant55@gmail.com>
2025-06-11 11:13:59 +0530

    Removed maiin branch files from new branch

--- Program3.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-public class Program3{
-   public static void main(String[] args){		
-	Scanner sc=new Scanner(System.in);
-	double a=sc.nextDouble();
-	double newtemp=(a*(9/5))+32;
- 	System.out.println("the new temp is"+" "+newtemp);
-}
-}
