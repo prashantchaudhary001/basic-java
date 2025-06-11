f6bff6d4bc5b66413e867c9f08888f57d47ac487
prashantchaudhary001 <jaatprashant55@gmail.com>
2025-06-11 11:07:10 +0530

    Re-added day1 folder as normal directory

---
+++ day1/AgeFind.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class AgeFind{
+   public static void main(String[] args){		
+	int birthYear=2000;
+	int currAgeYear=2024;
+
+	int currage=currAgeYear-birthYear;
+	System.out.println(currage);
+}
+}
