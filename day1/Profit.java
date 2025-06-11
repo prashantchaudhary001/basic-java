0eadb4b291038bb9eb56689905cf517702d0f978
prashantchaudhary001 <jaatprashant55@gmail.com>
2025-06-11 12:04:23 +0530

    fourth commit

---
+++ day1/Profit.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Profit{
+   public static void main(String[] args){		
+	int costPrice=129;
+	int sellingPrice=191;
+	int profit=sellingPrice-costPrice;
+	float profitPercentage=(profit/costPrice)*100;
+	System.out.println(profitPercentage);
+}
+}
