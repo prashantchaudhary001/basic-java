e2b1b39ab2e74df1652bda350ec80eca9384a104
prashantchaudhary001 <jaatprashant55@gmail.com>
2025-06-11 09:45:14 +0530

    Added Day1 programming elements

---
+++ AgeFind.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class AgeFind{
+   public static void main(String[] args){		
+	int birthYear=2000;
+	int currAgeYear=2024;
+
+	int age=currAgeYear-birthYear;
+	System.out.println(age);
+}
+}
